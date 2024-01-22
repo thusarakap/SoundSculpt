4c7273d1e266269a560a13a0f610ee30717b1975
Thusaraka Palliyeguru <87273240+box0de0ex@users.noreply.github.com>
2024-01-23 02:36:19 +0530

    Created Index Page
    Created Index page, styled using CSS.
    Added custom icon.
    (Created Start and login pages but did not style.)

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # SoundSculpt
 PC software to calibrate speakers by utilising a smartphone.
+
+Attributes:
+forward icons PNG Designed By EncoderXSolutions from https://pngtree.com/freepng/vector-forward-icon_4184777.html?sol=downref&id=bef
---
+++ main.js
@@ -0,0 +1,33 @@
+// main.js
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 700,
+    icon: path.join(__dirname, 'resources/images/SoundSulptIcon.png'),
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  // Load the start screen
+  mainWindow.loadFile('index.html');
+
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', function () {
+  if (mainWindow === null) createWindow();
+});
