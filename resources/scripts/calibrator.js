fcdb038fcef8080bce3528f5471a6b17dfa01211
Thusaraka Palliyeguru <87273240+thusarakap@users.noreply.github.com>
2024-03-24 18:04:35 +0530

    Update calibrator.js
    Added button to download calibrated profile

--- resources/scripts/calibrator.js
+++ resources/scripts/calibrator.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 // Select the canvas
 const canvas = document.getElementById('waveformCanvas');
 const canvasContext = canvas.getContext('2d');
@@ -99,3 +102,51 @@ cancelButton.addEventListener('click', function() {
     // Reset the current time to 0
     audio.currentTime = 0;
 });
+
+
+
+document.getElementById('getCalibratedProfile').addEventListener('click', function() {
+    // Get 'userId' from the localStorage
+    const userId = localStorage.getItem('userId');
+
+    // Get user data from Realtime Database
+    const db = firebase.database();
+    db.ref('users/' + userId).once('value').then((snapshot) => {
+      if (snapshot.exists()) {
+        let userData = snapshot.val();
+        console.log("dBValues:", userData.dBValues);
+        console.log("Speaker Type:", userData.speakerType);
+    
+        // Convert dBValues to an array of floats
+        if (typeof userData.dBValues === 'string') {
+            userData.dBValues = userData.dBValues.split(',').map(value => parseFloat(value));
+        }
+    
+        // Save userData to a JSON file
+        fs.writeFile(path.join(__dirname, '..', 'resources', 'data', 'userData.json'), JSON.stringify(userData, null, 2), (err) => {
+            if (err) {
+                console.error("Error writing file:", err);
+            }
+        });
+    
+        // Check if dBValues and speakerType are not empty
+        if (userData.dBValues && userData.speakerType) {
+            // If they are not empty, redirect to home.html
+            location.replace("home.html");
+        } else {
+            // If they are empty, redirect to calibration.html
+            location.replace("calibrator.html");
+        }
+    } else {
+        // If user doesn't exist in the database, redirect to calibration.html
+        location.replace("calibrator.html");
+    }
+    }).catch((error) => {
+        console.log("Error getting user data:", error);
+        passwordField.insertAdjacentHTML('afterend', `<p class="error-message" style="margin-left: 1px;">${error.message}</p>`);
+    });
+  })
+  .catch((error) => {
+    const errorMessage = error.message;
+    passwordField.insertAdjacentHTML('afterend', `<p class="error-message" style="margin-left: 1px;">${errorMessage}</p>`);
+  });
